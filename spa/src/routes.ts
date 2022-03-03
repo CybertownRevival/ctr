6d186afdcd59eb256f916897e223091c971fd72c
David Burleson <davidburleson@gmail.com>
2022-03-03 09:00:17 +0000

    #21 fix code style in routes.ts

--- spa/src/routes.ts
+++ spa/src/routes.ts
@@ -4,16 +4,27 @@ import SignupPage from "./pages/SignupPage.vue";
 import LogoutPage from "./pages/LogoutPage.vue";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.vue";
 import PasswordResetPage from "./pages/PasswordResetPage.vue";
-
 import WorldBrowserPage from "./pages/world-browser/WorldBrowserPage.vue";
 
 export default [
-  { path: "/", component: HomePage, name: "home", meta: { title: 'Welcome to Cybertown' } },
+  { path: "/", component: HomePage, name: "home", meta: {
+    title: "Welcome to Cybertown",
+  } },
   { path: "/place/:id", component: WorldBrowserPage, name: "world-browser" },
-  { path: "/login", component: LoginPage, name: "login", meta: { title: 'Login' } },
-  { path: "/signup", component: SignupPage, name: "signup", meta: { title: 'Immigrate' } },
-  { path: "/logout", component: LogoutPage, name: "logout", meta: { title: 'Logout' } },
-  { path: "/forgot", component: ForgotPasswordPage, name: "forgot", meta: { title: 'Forgot Password' } },
-  { path: "/password_reset", component: PasswordResetPage, name: "password_reset", meta: { title: 'Password Reset' } },
+  { path: "/login", component: LoginPage, name: "login", meta: {
+    title: "Login",
+  } },
+  { path: "/signup", component: SignupPage, name: "signup", meta: {
+    title: "Immigrate",
+  } },
+  { path: "/logout", component: LogoutPage, name: "logout", meta: {
+    title: "Logout",
+  } },
+  { path: "/forgot", component: ForgotPasswordPage, name: "forgot", meta: {
+    title: "Forgot Password",
+  } },
+  { path: "/password_reset", component: PasswordResetPage, name: "password_reset", meta: {
+    title: "Password Reset",
+  } },
 ];
 
