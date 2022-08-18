372bc111b1fb82f2a3014194d5e3adf96918fbdc
Ryan Boykin <ryan@theyapps.com>
2022-08-17 19:15:27 -0500

    Corrected tabs

--- spa/src/components/modals/Modal.vue
+++ spa/src/components/modals/Modal.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
     <div class="absolute
-        top-10
-        bottom-10
-        left-1/2
-        transform
-        -translate-x-1/2
-        border-2
-        border-gray-300
-        max-w-full
-        shadow-lg
-        bg-black
-        overflow-y-hidden" style="width:1024px;">
+      top-10
+      bottom-10
+      left-1/2
+      transform
+      -translate-x-1/2
+      border-2
+      border-gray-300
+      max-w-full
+      shadow-lg
+      bg-black
+      overflow-y-hidden" style="width:1024px;">
       <div class="flex
-      flex-row-reverse
-      bg-gradient-to-r
-      from-indigo-500
-      to-blue-500
-      p-1
-      border-b-2
-      border-gray-400">
+        flex-row-reverse
+        bg-gradient-to-r
+        from-indigo-500
+        to-blue-500
+        p-1
+        border-b-2
+        border-gray-400">
         <slot name="header"></slot>
         
       </div>
