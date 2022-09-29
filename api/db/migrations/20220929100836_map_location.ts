2991e2fa174509fbf3419d6349d40ff3f187df95
David Burleson <davidburleson@gmail.com>
2022-09-29 18:24:19 +0100

    #72 migration for adding map_location table

---
+++ api/db/migrations/20220929100836_map_location.ts
@@ -0,0 +1,43 @@
+import { Knex } from 'knex';
+const COLLATE = 'utf8mb4_unicode_ci';
+
+
+export async function up(knex: Knex): Promise<void> {
+  if (!await knex.schema.hasTable('map_location')) {
+    await knex.schema.createTable('map_location', table => {
+      console.log('Creating map_location table');
+
+      table.collate(COLLATE);
+
+      table.integer('parent_place_id')
+        .notNullable();
+
+      table.integer('place_id')
+        .notNullable();
+
+      table.integer('location')
+        .notNullable();
+
+      table.string('map_image');
+
+      table.boolean('available')
+        .defaultTo(false)
+        .notNullable();
+
+      table.primary(['parent_place_id','location']);
+
+    });
+  }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  if (await knex.schema.hasTable('map_location')) {
+    console.log('dropping map_location table');
+
+    await knex.raw('SET foreign_key_checks = 0');
+    await knex.schema.dropTable('map_location');
+    await knex.raw('SET foreign_key_checks = 1');
+  }
+}
+
