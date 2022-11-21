e5aa9e04e3af995a14de98c979d048a5c0c0de04
Mike <mike@actionsoftware.co>
2022-11-20 23:03:43 -0600

    Added jest for tests on API

---
+++ api/jest.config.js
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testMatch: ['**/*.spec.ts'],
+};
