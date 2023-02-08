6b475a5257dc3398a25a3bb2de4f4617b65ce5ba
David Burleson <davidburleson@gmail.com>
2023-02-08 18:00:36 +0000

    #75 remove reference to db name in home refund transaction

--- api/db/migrations/20230202151154_home_refund_transaction.ts
+++ api/db/migrations/20230202151154_home_refund_transaction.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   console.log('applying change');
-  await knex.raw("ALTER TABLE `cybertown`.`transaction` CHANGE COLUMN `reason` `reason`" +
+  await knex.schema.raw("ALTER TABLE `transaction` CHANGE COLUMN `reason` `reason`" +
     " ENUM('daily-credit'," +
     " 'home-purchase', 'item-purchase', 'member-to-member', 'system-to-member', 'home-refund') NOT NULL");
 }
@@ -10,7 +10,7 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   console.log('applying change');
-  await knex.raw("ALTER TABLE `cybertown`.`transaction` CHANGE COLUMN `reason` `reason`" +
+  await knex.schema.raw("ALTER TABLE `transaction` CHANGE COLUMN `reason` `reason`" +
     " ENUM('daily-credit'," +
     " 'home-purchase', 'item-purchase', 'member-to-member', 'system-to-member') NOT NULL");
 }
