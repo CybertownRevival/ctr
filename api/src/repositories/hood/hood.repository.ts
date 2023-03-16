258173d07d83db444a6ca0e8e37ea95051859b45
David Burleson <davidburleson@gmail.com>
2023-03-16 10:47:34 +0000

    #173 hood controller refactored to use service->repo for db

--- api/src/repositories/hood/hood.repository.ts
+++ api/src/repositories/hood/hood.repository.ts
@@ -1,6 +1,7 @@
 import { Service } from 'typedi';
 
 import { Db } from '../../db/db.class';
+import {knex} from '../../db';
 import {Place} from '../../types/models';
 
 @Service()
@@ -10,6 +11,19 @@ export class HoodRepository {
 
   public async find(hoodId: number): Promise<Place> {
     return this.db.place.where({'type':'hood','id':hoodId}).first();
+  }
+
+  public async getBlocks(hoodId: number): Promise<any> {
+    return knex
+      .select('place.id',
+        'place.name',
+        'map_location.location',
+      )
+      .from('place')
+      .innerJoin('map_location', 'map_location.place_id', 'place.id')
+      .where('map_location.parent_place_id', hoodId)
+      .orderBy('map_location.location');
+
   }
 
 
