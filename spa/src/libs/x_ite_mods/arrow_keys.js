d5060da97d7d4e490f2a6b5a7b842463bd097837
Sgeo <sgeoster@gmail.com>
2023-03-18 04:45:19 -0400

    Fix X_ITE canvas size, fix syntax for some mods, other fixes.
    bxx_auth.js fixes not tested.

--- spa/src/libs/x_ite_mods/arrow_keys.js
+++ spa/src/libs/x_ite_mods/arrow_keys.js
@@ -1,55 +1,56 @@
 (function () {
 
-  X3D.require(["x_ite/Browser/Navigation/WalkViewer"], function (WalkViewer) {
-    let originalInitialize = WalkViewer.prototype.initialize;
+  const WalkViewer = X3D.require("x_ite/Browser/Navigation/WalkViewer");
 
-    WalkViewer.prototype.keydown = function (e) {
-      if (e.keyCode > 36 && e.keyCode < 41 || e.keyCode == 17) {
-        if (this.keyx == 0 && this.keyy == 0) {
-          this.fromVector.set(0, 0, 0);
-          this.toVector.assign(this.fromVector);
-          this.getFlyDirection(this.fromVector, this.toVector, this.direction);
-          this.getBrowser().setCursor("MOVE");
-          this.addFly();
-        }
-        if (e.keyCode == 37) this.keyx = -50;  // Left
-        if (e.keyCode == 38) this.keyy = -100; // Up
-        if (e.keyCode == 39) this.keyx = 50;   // Right
-        if (e.keyCode == 40) this.keyy = 100;  // Down
-        if (this.getBrowser().getControlKey())
-          this.toVector.set(this.keyx, this.keyy / 4, 0);
-        else
-          this.toVector.set(this.keyx, 0, this.keyy);
-        this.getFlyDirection(this.fromVector, this.toVector, this.direction);
-      }
-    }
+  let originalInitialize = WalkViewer.prototype.initialize;
 
-    WalkViewer.prototype.keyup = function (e) {
-      if (e.keyCode == 37 || e.keyCode == 39) this.keyx = 0;
-      if (e.keyCode == 38 || e.keyCode == 40) this.keyy = 0;
+  WalkViewer.prototype.keydown = function (e) {
+    if (e.keyCode > 36 && e.keyCode < 41 || e.keyCode == 17) {
       if (this.keyx == 0 && this.keyy == 0) {
-        event.preventDefault();
-        this.event = null;
-        this.button = -1;
-        this.disconnect();
-        this.getBrowser().setCursor("DEFAULT");
-        this.removeCollision();
-        this.isActive_ = false;
-      } else {
-        this.toVector.set(this.keyx, 0, this.keyy);
+        this.fromVector.set(0, 0, 0);
+        this.toVector.assign(this.fromVector);
         this.getFlyDirection(this.fromVector, this.toVector, this.direction);
+        this.getBrowser().setCursor("MOVE");
+        this.addFly();
       }
+      if (e.keyCode == 37) this.keyx = -50;  // Left
+      if (e.keyCode == 38) this.keyy = -100; // Up
+      if (e.keyCode == 39) this.keyx = 50;   // Right
+      if (e.keyCode == 40) this.keyy = 100;  // Down
+      if (this.getBrowser().getControlKey())
+        this.toVector.set(this.keyx, this.keyy / 4, 0);
+      else
+        this.toVector.set(this.keyx, 0, this.keyy);
+      this.getFlyDirection(this.fromVector, this.toVector, this.direction);
     }
+  }
 
-    WalkViewer.prototype.initialize = function () {
-      var browser = this.getBrowser();
-      var element = browser.getElement();
-      this.keyx = 0;
-      this.keyy = 0;
-      element.bind('keydown.WalkViewer', this.keydown.bind(this));
-      element.bind('keyup.WalkViewer', this.keyup.bind(this));
-      originalInitialize.call(this);
+  WalkViewer.prototype.keyup = function (e) {
+    if (e.keyCode == 37 || e.keyCode == 39) this.keyx = 0;
+    if (e.keyCode == 38 || e.keyCode == 40) this.keyy = 0;
+    if (this.keyx == 0 && this.keyy == 0) {
+      event.preventDefault();
+      this.event = null;
+      this.button = -1;
+      this.disconnect();
+      this.getBrowser().setCursor("DEFAULT");
+      this.removeCollision();
+      this.isActive_ = false;
+    } else {
+      this.toVector.set(this.keyx, 0, this.keyy);
+      this.getFlyDirection(this.fromVector, this.toVector, this.direction);
     }
+  }
 
-  })
+  WalkViewer.prototype.initialize = function () {
+    console.log("Initializing WalkViewer");
+    var browser = this.getBrowser();
+    var element = browser.getElement();
+    this.keyx = 0;
+    this.keyy = 0;
+    element.bind('keydown.WalkViewer', this.keydown.bind(this));
+    element.bind('keyup.WalkViewer', this.keyup.bind(this));
+    originalInitialize.call(this);
+  }
+
 })();
--- spa/src/libs/x_ite_mods/bxx_speed_avatar.js
+++ spa/src/libs/x_ite_mods/bxx_speed_avatar.js
@@ -1,12 +1,11 @@
 (function () {
     // Blaxxun Speed & Avatar Size Defaults
-    X3D.require(["x_ite/Components/Navigation/NavigationInfo"], function (NavigationInfo) {
-        var nav = NavigationInfo.prototype.fieldDefinitions.index;
-        //nav['speed'].value.set(1);
-        //nav['visibilityLimit'].value.set(450);
-        //nav['headlight'].value.set(false);
-        //nav['avatarSize'].value.set([0.25, 1.75, 0.75, 0]);
-        nav['type'].value.set(["WALK", "FLY"]); // Only allowing the Walk and Fly viewers
-    });
+    const NavigationInfo = X3D.require("x_ite/Components/Navigation/NavigationInfo");
+    var nav = NavigationInfo.prototype.getFieldDefinitions();
+    //nav.get('speed').value.set(1);
+    //nav.get('visibilityLimit').value.set(450);
+    //nav.get('headlight').value.set(false);
+    //nav.get('avatarSize').value.set([0.25, 1.75, 0.75, 0]);
+    nav.get('type').value.set(["WALK", "FLY"]); // Only allowing the Walk and Fly viewers
 
 })();
