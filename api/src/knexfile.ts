981011d16e9455c92f649c741366b473951c6dcf
David Burleson <45258503+dburleson@users.noreply.github.com>
2023-05-31 17:18:15 +0100

    adding charset to production db config

--- api/src/knexfile.ts
+++ api/src/knexfile.ts
@@ -36,6 +36,7 @@ const config: { [key: string]: Knex.Config } = {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_DATABASE,
+      charset: 'utf8mb4',
     },
     pool: {
       min: 2,
