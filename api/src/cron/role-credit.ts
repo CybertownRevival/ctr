fa6ab7b83325c80175af159575b386c286a21928
D <45258503+dburleson@users.noreply.github.com>
2023-11-30 18:14:24 +0000

    Update role-credit.ts

--- api/src/cron/role-credit.ts
+++ api/src/cron/role-credit.ts
@@ -4,10 +4,10 @@ import { RoleAssignmentService } from '../services';
 module.exports = async () => {
   console.log('CRON[role-credit]: Running...');
   const roleAssignmentService = Container.get(RoleAssignmentService);
-  const batch = await roleAssignmentService.getMembersDueRoleCredit(1000);
+  const batch = await roleAssignmentService.getMembersDueRoleCredit(20);
   console.log(`CRON[role-credit]: ${  batch.length  } to process...`);
-  batch.forEach(row => {
-    roleAssignmentService.giveWeeklyRoleCredit(
+  batch.forEach(async (row) => {
+    await roleAssignmentService.giveWeeklyRoleCredit(
       row.member_id,
       row.xp,
       row.wallet_id,
