95bc6130c439c6739f53e679ef1654e6bb011b4d
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-04-16 07:24:26 -0500

    Update ObjectProperties.vue

--- spa/src/pages/ObjectProperties.vue
+++ spa/src/pages/ObjectProperties.vue
@@ -61,35 +61,32 @@
         style="min-width: 450px;">
           <span id=name class="font-bold text-3xl">{{ this.name }}</span>
           <div>Click <span class="font-bold cursor-pointer" style="color:lime" @click="changeACtive()">HERE</span> to view the object in 3D!</div>
-          <span class="flex" v-show="this.displayModel">Created by {{ this.creator }}</span>
           <span class="h-1.5"></span>
           <span v-if="this.placeId === 0 && this.ownerId === this.sessionId">This object is located in your backpack.</span>
           <span v-else-if="this.ownerId !== this.sessionId && this.placeId === 0">This object is located in {{ this.memberUsername }}'s backpack.</span>
           <span class="h-1.5"></span>
           <span>You have {{ this.walletBalance }} CC's.</span>
           <span class="h-5"></span>
-          <span v-show="this.displayModel">Quantity: {{ this.qty }}</span>
           <span v-show="this.price !== null && this.price !== ''">Price: {{ this.price }} CC's</span>
           <span v-show="this.price !== null && this.price !== '' && this.buyer !=='' && this.buyer !==null">Reserved for {{ this.buyer }}</span>
           <span class="h-5"></span>
-          <div class="objectOwner grid gap-2" v-show="this.sessionId === this.ownerId">
-            <div class="flex"><div style="min-width: 70px;">Name: </div><span id="nameChange"></span></div>
-            <div class="flex"><div style="min-width: 70px;">Price: </div><span id="priceChange"></span></div>
-            <div id="qty" class="hidden"><div style="min-width: 70px;">Qty: </div><span id="qtyChange"></span></div>
-            <div class="flex"><div style="min-width: 70px;">Buyer: </div><span id="buyerChange"></span></div>
+          <div class="objectOwner grid gap-2" v-show="canModify">
+            <div class="flex"><div style="min-width: 70px;">Name: </div><input style="color:black;" type="text" id="objectName" :value="name" @input="event => name = event.target.value"/></div>
+            <div class="flex"><div style="min-width: 70px;">Price: </div><input style="color:black;" type="text" id="objectPrice" maxlength="7" :value="price" @input="event => price = event.target.value"/></div>
+            <div class="flex"><div style="min-width: 70px;">Buyer: </div><input style="color:black;" type="text" id="objectBuyer" :value="buyer" @input="event => buyer = event.target.value"/></div>
           </div>
       </div>
     </div>
     <span class="flex w-full justify-center text-red-600 mt-10" v-show="error">{{ this.error }}</span>
     <div class="flex justify-center">
       
-      <button  type="button" class="btn mx-1 mt-10" @click="changeDetails()" v-if="this.sessionId === this.ownerId">Update</button>
+      <button  type="button" class="btn mx-1 mt-10" @click="changeDetails()" v-show="this.canModify">Update</button>
         <button  type="button" class="btn mx-1 mt-10" 
           v-if="
           this.sessionId !== this.ownerId && this.price !== '' && (this.buyer === '' || this.buyer === this.$store.data.user.username) && this.walletBalance >= this.price" @click="buy()">
           Buy
         </button>
-      <button type="button" class="btn mx-1 mt-10" onclick="window.close()">Close</button></div>
+      <button type="button" class="btn mx-1 mt-10" @click="close()">Close</button></div>
     </div>
   </div>
 </template>
@@ -116,11 +113,8 @@ export default Vue.extend({
       success: "",
       walletBalance: null,
       canModify: false,
-      displayModel: false,
       price: null,
       buyer: null,
-      qty: null,
-      creator: null,
       active: "properties",
     };
   },
@@ -144,7 +138,7 @@ methods: {
         this.name = object.object_name;
         this.price = object.object_price;
         this.buyer = object.object_buyer;
-
+        this.memberUsername = object.username;
         if(this.sessionId === this.ownerId){
           this.canModify = true;
         }
@@ -154,33 +148,16 @@ methods: {
         if(this.buyer === null){
           this.buyer = '';
         }
-        if(this.displayModel){
-          const qty = document.getElementById('qty');
-          const qtyInput = document.getElementById('qtyChange');
-          qty.style.display= "flex";
-          qtyInput.innerHTML= `<input style="color:black;" type="input" id="objectPrice" value="${this.qty}" />`;
-        }
-        this.loadData();
-        this.getOwner();
       });
-  },
-  async getOwner(){
-    const ownerInfo = await this.$http.get(`/member/info/${this.ownerId}`);
-    this.memberUsername = ownerInfo.data.memberInfo.username;
   },
   reload(){
     window.location.reload();
   },
-  async loadData(){
-    const nameInput = document.getElementById("nameChange");
-    const priceInput = document.getElementById("priceChange");
-    const buyerInput = document.getElementById("buyerChange");
-
-    nameInput.innerHTML = `<input style="color:black;" type="text" v-model="objectName" id="objectName" value="${this.name}"/>`;
-    priceInput.innerHTML = `<input style="color:black;" type="input" id="objectPrice" value="${this.price}" maxlength='7' />`;
-    buyerInput.innerHTML = `<input style="color:black;" type="input" id="objectBuyer" value="${this.buyer}" />`;
+  close(){
+    window.close();
   },
   changeDetails() {
+    console.log(this.name, this.price, this.buyer)
     this.name = (<HTMLInputElement>document.getElementById('objectName')).value.replace(/[^0-9a-zA-Z \-\[\]()]/g, '');
     this.price = (<HTMLInputElement>document.getElementById('objectPrice')).value.replace(/[^0-9]/g, '');
     const badwords = require("badwords-list");
@@ -188,7 +165,6 @@ methods: {
     if (this.name.match(bannedwords)) {
       alert('You can not use this type of language on CTR!');
       this.name = this.originalName;
-      this.loadData();
     }
     if(this.name === ""){
       this.name = this.originalName;
