f99ce26778196a4f97270696912b6e71a39e8714
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-05-14 10:23:43 -0500

    Update index.ts

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -2,6 +2,7 @@ export * from './admin/admin.services';
 export * from './avatar/avatar.service';
 export * from './block/block.service';
 export * from './colony/colony.service';
+export * from './fleamarket/fleamarket.service';
 export * from './home/home.service';
 export * from './hood/hood.service';
 export * from './mall/mall.service';
