68430849b2f7889e98e2d963d41c8bf2cbb46f41
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-05-28 14:54:48 -0500

    Add files via upload

---
+++ api/db/migrations/20240528145929_object_add_limit_field.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+const columnName = 'limit';
+export async function up(knex: Knex): Promise<void> {
+  if (!await knex.schema.hasColumn('object', columnName)) {
+    console.log(`Adding ${columnName} column to the object table`);
+    await knex.schema.alterTable('object', table => {
+      table.string(columnName)
+        .after('quantity')
+        .defaultTo(null);
+    });
+  }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  if (await knex.schema.hasColumn('object', columnName)){
+    console.log(`Dropping ${columnName} column from the object table`);
+    await knex.schema.alterTable('object', table => {
+      table.dropColumn(columnName);
+    });
+  }
+}
+
