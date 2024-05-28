11ce5fbfe31d50fa36e540e4c030faf51cbfefea
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-05-28 15:06:49 -0500

    Add files via upload

--- api/src/services/admin/admin.services.ts
+++ api/src/services/admin/admin.services.ts
@@ -7,6 +7,7 @@ import {
   RoleAssignmentRepository,
   RoleRepository,
   AvatarRepository,
+  PlaceRepository,
 } from '../../repositories';
 
 @Service()
@@ -18,6 +19,7 @@ export class AdminService {
    private roleAssignmentRepository: RoleAssignmentRepository,
    private roleRepository: RoleRepository,
    private avatarRespository: AvatarRepository,
+   private placeRepository: PlaceRepository,
   ) {}
   
   public async addBan(ban_member_id, time_frame, type, assigner_member_id, reason): Promise<void> {
@@ -92,6 +94,19 @@ export class AdminService {
     const total = await this.avatarRespository.totalByStatus(status);
     return {
       avatars: avatars,
+      total: total,
+    };
+  }
+
+  public async updatePlaces(id: number, column: string, content: string): Promise<any> {
+    await this.placeRepository.updatePlaces(id, column, content);
+  }
+
+  public async searchPlaces(type: string, limit: number, offset: number): Promise<any> {
+    const places = await this.placeRepository.findByType(type, limit, offset);
+    const total = await this.placeRepository.totalByType(type);
+    return {
+      places: places,
       total: total,
     };
   }
