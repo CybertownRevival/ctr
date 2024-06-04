88feb40cc750a9150b6eabf0b90ef63fe16585ac
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-06-04 14:24:18 -0500

    Update Migration File
    Updated migration file for adding the limit column to the object table. Changed data type to an unsigned integer.

--- api/db/migrations/20240528145929_object_add_limit_field.ts
+++ api/db/migrations/20240528145929_object_add_limit_field.ts
@@ -5,7 +5,8 @@ export async function up(knex: Knex): Promise<void> {
   if (!await knex.schema.hasColumn('object', columnName)) {
     console.log(`Adding ${columnName} column to the object table`);
     await knex.schema.alterTable('object', table => {
-      table.string(columnName)
+      table.integer(columnName)
+        .unsigned()
         .after('quantity')
         .defaultTo(null);
     });
