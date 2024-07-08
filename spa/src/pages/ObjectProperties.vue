9fd514f5f9c606152485091bcc5b1170cc472191
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-07-08 15:48:08 -0500

    Update ObjectProperties.vue

--- spa/src/pages/ObjectProperties.vue
+++ spa/src/pages/ObjectProperties.vue
@@ -180,7 +180,7 @@ methods: {
     window.close();
   },
   changeDetails() {
-    this.name = (<HTMLInputElement>document.getElementById('objectName')).value.replace(/[^0-9a-zA-Z \-\[\]()]/g, '');
+    this.name = (<HTMLInputElement>document.getElementById('objectName')).value.replace(/[^0-9a-zA-Z \-\[\]\/()]/g, '');
     this.price = (<HTMLInputElement>document.getElementById('objectPrice')).value.replace(/[^0-9]/g, '');
     const badwords = require("badwords-list");
     const bannedwords = badwords.regex;
