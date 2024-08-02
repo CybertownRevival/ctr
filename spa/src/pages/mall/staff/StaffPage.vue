89beac481b1d29c86d8712b1dba011758a69f105
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-08-01 20:36:22 -0500

    Update StaffPage.vue
    Added link back to the main mall from the mall staff panel.

--- spa/src/pages/mall/staff/StaffPage.vue
+++ spa/src/pages/mall/staff/StaffPage.vue
@@ -8,6 +8,8 @@
       <div class="mb-2"><router-link class="btn-ui" :to="{name: 'MallStocked'}">Stocked</router-link></div>
       <div class="mb-2"><router-link class="btn-ui" :to="{name: 'MallSoldOut'}">Out of Stock</router-link></div>
       <div class="mb-2"><router-link class="btn-ui" :to="{name: 'MallObjectSearch'}">Search</router-link></div>
+      <br />
+      <div class="mb-2"><router-link class="btn-ui" :to="{path: '/place/mall'}">Return to Mall</router-link></div>
     </div>
     <div class="w-11/12 h-full p-1 overflow-y-auto"><router-view /></div>
 </div>
