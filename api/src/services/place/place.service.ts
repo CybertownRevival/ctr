0d842847d5971dff3432d2249fdf9c222094265e
smile07 <smile0711@sbcglobal.net>
2024-08-04 13:22:50 -0500

    Add canAdmin endpoint to place routes
    Implemented a new 'canAdmin' endpoint to check administrative permissions within `place.routes.ts`. Added necessary service methods and controller logic to handle role-based admin validation for places.

--- api/src/services/place/place.service.ts
+++ api/src/services/place/place.service.ts
@@ -1,6 +1,11 @@
 import { Service } from 'typedi';
 
-import { PlaceRepository, ObjectInstanceRepository } from '../../repositories';
+import {
+  PlaceRepository,
+  ObjectInstanceRepository,
+  RoleRepository,
+  RoleAssignmentRepository,
+} from '../../repositories';
 import { Place, ObjectInstance } from '../../types/models';
 
 /** Service for dealing with blocks */
@@ -9,8 +14,55 @@ export class PlaceService {
   constructor(
     private placeRepository: PlaceRepository,
     private objectInstanceRepository: ObjectInstanceRepository,
+    private roleRepository: RoleRepository,
+    private roleAssignmentRepository: RoleAssignmentRepository,
   ) {}
-
+  
+  public async canAdmin(slug: string, placeId: number, memberId: number):
+   Promise<boolean> {
+    const placeRoleId = await this.findRoleIdsBySlug(slug);
+    const roleAssignments = await this.roleAssignmentRepository.getByMemberId(memberId);
+    
+    //check if admin even if there is no assigned roles for the place
+    if (!placeRoleId) {
+      if (
+        roleAssignments.find(assignment => {
+          return (
+            [
+              this.roleRepository.roleMap.Admin,
+              this.roleRepository.roleMap.CityMayor,
+              this.roleRepository.roleMap.DeputyMayor,
+            ].includes(assignment.role_id)
+          );
+        })
+      ) {
+        return true;
+      }
+    }
+    
+    //check if worker or admin with an assigned roles for the place
+    if (placeRoleId) {
+      if (
+        roleAssignments.find(assignment => {
+          return (
+            [
+              this.roleRepository.roleMap.Admin,
+              this.roleRepository.roleMap.CityMayor,
+              this.roleRepository.roleMap.DeputyMayor,
+            ].includes(assignment.role_id) ||
+          ([
+            placeRoleId.owner,
+            placeRoleId.deputy,
+          ].includes(assignment.role_id) &&
+           assignment.place_id === placeId)
+          );
+        })
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
   public async findById(placeId: number): Promise<Place> {
     return await this.placeRepository.findById(placeId);
   }
@@ -29,5 +81,43 @@ export class PlaceService {
 
   public async updatePlaces(id: number, column: string, content: string): Promise<any> {
     await this.placeRepository.updatePlaces(id, column, content);
+  }
+  
+  private async findRoleIdsBySlug(slug: string): Promise<{owner: number, deputy: number}> {
+    const roleId = {
+      bank: {
+        owner: this.roleRepository.roleMap.BankManager,
+        deputy: this.roleRepository.roleMap.BankCashier,
+      },
+      clubs: {
+        owner: this.roleRepository.roleMap.ClubsChief,
+        deputy: this.roleRepository.roleMap.ClubsDeputy,
+      },
+      employment: {
+        owner: this.roleRepository.roleMap.EmploymentChief,
+        deputy: this.roleRepository.roleMap.EmploymentDeputy,
+      },
+      eplex: {
+        owner: this.roleRepository.roleMap.ePlexChief,
+        deputy: this.roleRepository.roleMap.ePlexDeputy,
+      },
+      flea: {
+        owner: this.roleRepository.roleMap.FleaMarketChief,
+        deputy: this.roleRepository.roleMap.FleaMarketDeputy,
+      },
+      mall: {
+        owner: this.roleRepository.roleMap.BankManager,
+        deputy: this.roleRepository.roleMap.BankCashier,
+      },
+      outlands: {
+        owner: this.roleRepository.roleMap.OutlandsChief,
+        deputy: this.roleRepository.roleMap.OutlandsDeputy,
+      },
+      postoffice: {
+        owner: this.roleRepository.roleMap.PostOfficeManager,
+        deputy: this.roleRepository.roleMap.PostOfficeDeputy,
+      },
+    };
+    return roleId[slug];
   }
 }
