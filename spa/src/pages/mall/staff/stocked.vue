66476539f3847b8a8974b7fb814875f909b0d5c0
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2024-08-25 16:15:43 -0500

    Update stocked.vue

--- spa/src/pages/mall/staff/stocked.vue
+++ spa/src/pages/mall/staff/stocked.vue
@@ -138,7 +138,7 @@ export default Vue.extend({
         this.error = "Use whole numbers only!";
         return
       }
-      if(limit < quantity){
+      if(limit >= '1' && limit < quantity){
         this.error = "Limit cannot be less than the uploaded quantity."
         return
       }	
