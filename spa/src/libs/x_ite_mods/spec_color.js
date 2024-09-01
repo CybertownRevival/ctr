d420f33be3327ca29d5efed66f1f10ee48977dc3
Sgeo <sgeoster@gmail.com>
2024-09-01 01:55:59 -0400

    Fix spec_color.js * Method names * New method * data can sometimes be an <img> instead of pixel data

--- spa/src/libs/x_ite_mods/spec_color.js
+++ spa/src/libs/x_ite_mods/spec_color.js
@@ -1,5 +1,34 @@
 (function () {
 
+    async function getImgPixels(img) {
+        if(!img.complete) {
+            await new Promise((resolve, reject) => {
+                img.onload = resolve;
+                img.onerror = reject;
+            });
+        }
+        let canvas = new OffscreenCanvas(img.naturalWidth, img.naturalHeight);
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        return ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight).data;
+    }
+
+    function applyRgb(target, data) {
+        target.knownRgb = false;
+        let gotData = Promise.resolve(data);
+        if(data instanceof HTMLImageElement) {
+            gotData = getImgPixels(data);
+        }
+        gotData.then(data => {
+            for (let i = 0; i < data.length; i += 4) {
+                if (data[i] !== data[i + 1] || data[i + 1] !== data[i + 2]) {
+                    target.knownRgb = true;
+                    break;
+                }
+            }
+        });
+    }
+
     
     const Appearance = X3D.require("x_ite/Components/Shape/Appearance");
     let originalTraverse = Appearance.prototype.traverse;
@@ -20,16 +49,16 @@
     }
     
     const X3DTexture2DNode = X3D.require("x_ite/Components/Texturing/X3DTexture2DNode");
-    let originalSetTexture = X3DTexture2DNode.prototype.setTexture;
-    X3DTexture2DNode.prototype.setTexture = function (width, height, transparent, data, flipY) {
-        this.knownRgb = false;
-        for (let i = 0; i < data.length; i += 4) {
-            if (data[i] !== data[i + 1] || data[i + 1] !== data[i + 2]) {
-                this.knownRgb = true;
-                break;
-            }
-        }
-        return originalSetTexture.call(this, width, height, transparent, data, flipY);
+    let originalSetTextureData = X3DTexture2DNode.prototype.setTextureData;
+    X3DTexture2DNode.prototype.setTextureData = function (width, height, colorSpaceConversion, transparent, data){
+        applyRgb(this, data);
+        return originalSetTextureData.call(this, width, height, colorSpaceConversion, transparent, data);
+    };
+
+    let originalUpdateTextureData = X3DTexture2DNode.prototype.updateTextureData;
+    X3DTexture2DNode.prototype.updateTextureData = function(data) {
+        applyRgb(this, data);
+        return originalUpdateTextureData.call(this, data);
     };
 
 })();
