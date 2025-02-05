14e276786677351ea0222fea71785f0e5020949c
smile07 <smile0711@sbcglobal.net>
2025-02-04 23:44:19 -0600

    Add Club Directory 2D component and enforce 2D for clubdir
    Introduce a new 2D component (`clubdir/main2d.vue`) for the place Club Directory page with basic layout and navigation options. Update `WorldBrowserPage.vue` to enforce 2D mode when navigating to the "clubdir" location.

---
+++ spa/src/components/place/clubdir/main2d.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center">
+    <img src="/assets/img/place/clubdir/clubdir.jpg" style="width: 500px;"
+    class="mb-2"/>
+    <br><button class="btn-ui-inline mx-2">CLUBS LIST</button>
+    <router-link class="btn-ui-inline mx-2" :to="{name: 'create-club'}">CREATE A CLUB</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ClubDirMain2d",
+  data: () => {
+    return {};
+  },
+});
+</script>
+
