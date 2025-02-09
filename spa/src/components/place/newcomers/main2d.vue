32968220aa1d901f3a434d5527d7e45a8ab3a358
smile07 <smile0711@sbcglobal.net>
2025-02-09 13:00:30 -0600

    Add club directory functionality with pagination and seeding
    Introduce a club directory feature, including pagination controls, to the frontend. Add backend support for counting total clubs and adjust the service logic accordingly. Seed initial club data and include assets for the "Newcomers Club" page.

---
+++ spa/src/components/place/newcomers/main2d.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <img src="/assets/img/place/newcomers/newcomers.jpg" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "NewcomersMain2d",
+  data: () => {
+    return {};
+  },
+});
+</script>
+
