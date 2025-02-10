04e849a992dac37fddfa12f65f7a57d501cfe57c
smile0711 <98184756+smile0711@users.noreply.github.com>
2025-02-10 01:38:48 -0600

    Revert "Add club-member model, repository, and migration"
    This reverts commit d36bf2b8dd1e4020d70e31a26b19cb69e5eba587.

--- api/src/db/db.class.ts
+++ api/src/db/db.class.ts
@@ -16,9 +16,6 @@ export class Db {
   get avatar() {
     return this.knex<Models.Avatar, Models.Avatar[]>('avatar');
   }
-  get clubMember() {
-    return this.knex<Models.ClubMember, Models.ClubMember[]>('club_member');
-  }
   get home() {
     return this.knex<Models.Home, Models.Home[]>('home');
   }
--- api/src/types/models/club-member.model.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from './model';
-
-/** Defines an Club-Member object as stored in the db */
-export interface ClubMember extends Model {
-  place_id: number;
-  member_id: number;
-  status: number;
-  username: string;
-}
