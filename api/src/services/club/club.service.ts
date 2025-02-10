97b903f8eb3a60e38f618bdd832c3a4f72311b01
smile0711 <98184756+smile0711@users.noreply.github.com>
2025-02-10 01:38:48 -0600

    Revert "Add `private` column to `place` table and refactor club type logic"
    This reverts commit 7bc9556464ee870e0ba4c2040a83d52cf60b6a66.

--- api/src/services/club/club.service.ts
+++ api/src/services/club/club.service.ts
@@ -36,17 +36,16 @@ export class ClubService {
     if (clubCount >= 3) {
       throw new Error('You can only have upto 3 clubs');
     }
-    /*
-    * if club is private, private is true,
-    * else if public, private is false,
-    * else throw invalid type error
-    */
-    let isPrivate: boolean;
-    if (type === 'private_club') {
-      isPrivate = true;
-    } else if (type === 'public_club') {
-      isPrivate = false;
-    } else {
+    //if club is private, type is private_club
+    if (type === 'private') {
+      type = 'private_club';
+    }
+    //else if club is public, type is public_club
+    else if (type === 'public') {
+      type = 'public_club';
+    }
+    //else throw error
+    else {
       throw new Error('Invalid club type');
     }
     //information to make the place
@@ -54,7 +53,7 @@ export class ClubService {
       member_id: memberId,
       name: name,
       description: description,
-      private: isPrivate,
+      type: type,
     };
     //create the club and capture place id
     const placeId = await this.placeRepository.create(params);
