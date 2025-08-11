06c67c235ce4f934951d8ead44bd0ccfd8783c29
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2025-08-10 22:10:15 -0500

    Update admin.vue
    Adding link to avatar check back to the panel.

--- spa/src/pages/admin/admin.vue
+++ spa/src/pages/admin/admin.vue
@@ -3,6 +3,10 @@
     <div class="flex-col w-56 h-full border-r-2 border-white text-center">
       <div class="pt-3">Admin Panel</div>
       <div class="p-3"><hr></div>
+      <div class="mb-2" v-if="accessLevel.includes('admin')">
+        <router-link class="btn-ui" :to="{name: 'AvatarSearch'}">Avatars</router-link>
+      </div>
+      <br v-if="accessLevel.includes('admin')" />
       <div class="mb-2">
         <router-link class="btn-ui" v-if="accessLevel.includes('security')" :to="{name: 'CommunityOverview'}">Overview</router-link>
       </div>
