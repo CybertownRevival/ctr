40ef22628dbdcbf3bf95744692e8614a73732c5a
SalleeMatthew <132789789+SalleeMatthew@users.noreply.github.com>
2026-04-19 01:50:46 -0500

    Remove Comments
    Removed comments that were referencing other files not related to the blackmarket.

--- api/src/routes/blackmarket.routes.ts
+++ api/src/routes/blackmarket.routes.ts
@@ -2,10 +2,6 @@ import Router from 'express';
 
 import { blackmarketController } from '../controllers';
 
-/**
- * This file sets up routing for home routes.
- * @note All paths used here will be prepended with `/api/home`.
- */
 
 const blackmarketRoutes = Router();
 blackmarketRoutes.get('/can_admin', (request, response) => 
